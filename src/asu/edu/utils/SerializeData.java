69a131701394e7a8a7432125c918e5f2a3067e99
Pradeep <chaudharipradeep9@gmail.com>
2013-09-21 15:14:59 -0700

    MWDB UI Commit V0.1

---
+++ src/asu/edu/utils/SerializeData.java
@@ -0,0 +1,32 @@
+package asu.edu.utils;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Map;
+
+import asu.edu.math.ConstructGestureWords;
+
+public class SerializeData {
+	
+	private static void serialize(String outputFileName,Object object) throws IOException{
+		FileOutputStream fileOutputStream = new FileOutputStream(outputFileName);
+		ObjectOutputStream objectOutputStream  = new ObjectOutputStream(fileOutputStream);
+		objectOutputStream.writeObject(object);
+	}
+	
+	
+	
+	private  static Object deserialize(String inputFileName) throws ClassNotFoundException, IOException{
+		FileInputStream fileInputStream = new FileInputStream(inputFileName);
+		ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+		
+		return objectInputStream.readObject();
+	
+	
+	}
+	
+	
+}

b8a4bbfb86a0af04ed6c45b2d7219b8cce9f6ac5
Kedar Joshi <Kedar Joshi@Kedar>
2013-11-01 10:20:24 -0700

    reverting changes temporarily

--- mwdbphase2/src/com/asu/mwdb/matlab/MatlabObject.java
+++ mwdbphase2/src/com/asu/mwdb/matlab/MatlabObject.java
@@ -5,16 +5,10 @@ import matlabcontrol.MatlabProxy;
 import matlabcontrol.MatlabProxyFactory;
 
 public class MatlabObject {
-	
-	private static MatlabProxy _instance = null;
-	
-	private MatlabObject() {}
 
-	public static MatlabProxy getMatlabProxy() throws MatlabConnectionException {
+	public MatlabProxy getMatlabProxy() throws MatlabConnectionException {
 		MatlabProxyFactory factory = new MatlabProxyFactory();
-		if(_instance == null) {
-			_instance = factory.getProxy();
-		}
-		return _instance;
+		MatlabProxy proxy = factory.getProxy();
+		return proxy;
 	}
 }

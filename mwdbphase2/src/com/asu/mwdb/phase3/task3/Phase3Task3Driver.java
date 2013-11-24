9c52b8ae2b0212952841af5f4cc12ef17c295609
dlohith <dlohith@gmail.com>
2013-11-24 10:43:17 -0700

    Phase3 driver

---
+++ mwdbphase2/src/com/asu/mwdb/phase3/task3/Phase3Task3Driver.java
@@ -0,0 +1,41 @@
+package com.asu.mwdb.phase3.task3;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import com.asu.mwdb.utils.Utils;
+
+public class Phase3Task3Driver {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter database directory:");
+		String databaseDirectory = br.readLine();
+		System.out.println("Enter the file for gestures and labels:");
+		String gesturesLabels = br.readLine();
+		while(!Utils.isFilePresent(gesturesLabels)) {
+			System.out.println("File not found, please check the input");
+			gesturesLabels = br.readLine();
+		}
+		System.out.println("Enter value of K for KNN Classification:");
+		int kValue = Integer.parseInt(br.readLine());
+		
+		TrainingDataMaker trainingDataMaker = new TrainingDataMaker();
+		// return the test data files just to display results in output file in the following format
+		// filename - label
+		File[] fileNames = Utils.getFileOrder(databaseDirectory);
+		List<String> testDataFiles = trainingDataMaker.buildTrainingData(fileNames, gesturesLabels);
+		KNNClassification.knnClassify(gesturesLabels,testDataFiles, kValue);
+		
+		
+
+	}
+
+}

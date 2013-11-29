5d960817e8a48c08f517aae2fb67190b6032458c
dlohith <dlohith@gmail.com>
2013-11-29 12:09:14 -0700

    removed few comments and unwanted  Java files

--- mwdbphase2/src/com/asu/mwdb/phase3/task3/DecisionTreeClassification.java
+++ mwdbphase2/src/com/asu/mwdb/phase3/task3/DecisionTreeClassification.java
@@ -32,22 +32,34 @@ import com.asu.mwdb.utils.IConstants;
 
 public class DecisionTreeClassification {
 
-	public static void dtClassifyMatlab(MatlabProxy proxy, String databaseDirectory, String gesturesLabels, File[] fileNames, List<String> testDataFiles) throws MatlabInvocationException, IOException {
-		String trainingFile   = IConstants.DATA + File.separator + IConstants.TRAINING_FILE_NAME;
-		String labelsFile     = IConstants.DATA + File.separator + IConstants.LABELS_FILE_NAME;
-		String testingFile    = IConstants.DATA + File.separator + IConstants.TESTING_FILE_NAME;
-		String tempOutputPath = IConstants.DATA + File.separator + IConstants.OUTPUT_LABELS_TEMP_DT;
-		String dtOutputPath  = IConstants.DATA + File.separator + IConstants.OUTPUT_LABELS_DT;
-		String arg = "DecisionTreeClassifier('" + new File(trainingFile).getAbsolutePath() + "','" + new File(testingFile).getAbsolutePath() + "','"
-				+ new File(labelsFile).getAbsolutePath() + "','" + new File(tempOutputPath).getAbsolutePath() + "')";
+	public static void dtClassifyMatlab(MatlabProxy proxy,
+			String databaseDirectory, String gesturesLabels, File[] fileNames,
+			List<String> testDataFiles) throws MatlabInvocationException,
+			IOException {
+		String trainingFile = IConstants.DATA + File.separator
+				+ IConstants.TRAINING_FILE_NAME;
+		String labelsFile = IConstants.DATA + File.separator
+				+ IConstants.LABELS_FILE_NAME;
+		String testingFile = IConstants.DATA + File.separator
+				+ IConstants.TESTING_FILE_NAME;
+		String tempOutputPath = IConstants.DATA + File.separator
+				+ IConstants.OUTPUT_LABELS_TEMP_DT;
+		String dtOutputPath = IConstants.DATA + File.separator
+				+ IConstants.OUTPUT_LABELS_DT;
+		String arg = "DecisionTreeClassifier('"
+				+ new File(trainingFile).getAbsolutePath() + "','"
+				+ new File(testingFile).getAbsolutePath() + "','"
+				+ new File(labelsFile).getAbsolutePath() + "','"
+				+ new File(tempOutputPath).getAbsolutePath() + "')";
 		proxy.eval(arg);
-		
 
 		List<String> tempLabels = FileUtils.readLines(new File(tempOutputPath));
-		CSVWriter csvWriter = new CSVWriter(new OutputStreamWriter(new FileOutputStream(dtOutputPath)), ',', CSVWriter.NO_QUOTE_CHARACTER);
+		CSVWriter csvWriter = new CSVWriter(new OutputStreamWriter(
+				new FileOutputStream(dtOutputPath)), ',',
+				CSVWriter.NO_QUOTE_CHARACTER);
 		List<String[]> finalLabels = new ArrayList<String[]>();
 		int i = 0;
-		for(String testFile : testDataFiles) {
+		for (String testFile : testDataFiles) {
 			String[] line = new String[2];
 			line[0] = testFile;
 			line[1] = tempLabels.get(i++);
@@ -55,96 +67,82 @@ public class DecisionTreeClassification {
 		}
 		csvWriter.writeAll(finalLabels);
 		csvWriter.close();
-		
+
 	}
-	
-	public static void dtClassify(String testDatFile, String trainingDataFile) throws FileFormatException, IOException {
+
+	public static void dtClassify(String testDatFile, String trainingDataFile)
+			throws FileFormatException, IOException {
 		ItemSet learningSet = null;
 		ItemSet testSet = null;
-		try {
-		    learningSet = ItemSetReader.read(new FileReader(trainingDataFile));
-		    
-		}
-		catch(FileNotFoundException e) {
-		    System.err.println("File not found : " + trainingDataFile + ".");
-		    System.err.println("This file is included in the source " +
-				       "distribution of jaDti.  You can find it at " 
-				       );
-		    System.exit(-1);
-		}
 		
+		try {
+			learningSet = ItemSetReader.read(new FileReader(trainingDataFile));
+		} catch (FileNotFoundException e) {
+			System.err.println("File not found : " + trainingDataFile + ".");
+			System.exit(-1);
+		}
+
 		try {
 			testSet = ItemSetReader.read(new FileReader(testDatFile));
-		    
+
+		} catch (FileNotFoundException e) {
+			System.err.println("File not found : " + testDatFile + ".");
+			System.exit(-1);
 		}
-		catch(FileNotFoundException e) {
-		    System.err.println("File not found : " + testDatFile + ".");
-		    System.err.println("This file is included in the source " +
-				       "distribution of jaDti.  You can find it at " 
-				       );
-		    System.exit(-1);
-		}
-		
-		
+
 		AttributeSet attributes = learningSet.attributeSet();
 
-		
 		Vector testAttributesVector = new Vector();
 		List<Attribute> attributeList = attributes.addByName("attr");
-		for(Attribute att : attributeList){
+		for (Attribute att : attributeList) {
 			testAttributesVector.add(att);
 		}
-		
+
 		AttributeSet testAttributes = new AttributeSet(testAttributesVector);
-		SymbolicAttribute goalAttribute =
-		    (SymbolicAttribute) learningSet.attributeSet().findByName("label");
+		SymbolicAttribute goalAttribute = (SymbolicAttribute) learningSet
+				.attributeSet().findByName("label");
 
 		DecisionTree tree = buildTree(learningSet, testAttributes,
-					      goalAttribute);
-		System.out.println("something");
+				goalAttribute);
 		printDot(tree);
-		System.out.println("something");
-		for(double i = 0; i < testSet.size(); i++){
-			printGuess(testSet.item((int)i), tree);
+		for (double i = 0; i < testSet.size(); i++) {
+			printGuess(testSet.item((int) i), tree);
 		}
 	}
-	
-	  /*
-     * Build the decision tree.
-     */
-    static private DecisionTree buildTree(ItemSet learningSet, 
-					  AttributeSet testAttributes, 
-					  SymbolicAttribute goalAttribute) {
-	DecisionTreeBuilder builder = 
-	    new DecisionTreeBuilder(learningSet, testAttributes,
-				    goalAttribute);
-	
-	return builder.build().decisionTree();
-    }
-    
 
-    /*
-     * Prints a dot file content depicting a tree.
-     */
-    static private void printDot(DecisionTree tree) {
-	System.out.println((new DecisionTreeToDot(tree)).produce());
-    }
-    
+	/*
+	 * Build the decision tree.
+	 */
+	static private DecisionTree buildTree(ItemSet learningSet,
+			AttributeSet testAttributes, SymbolicAttribute goalAttribute) {
+		DecisionTreeBuilder builder = new DecisionTreeBuilder(learningSet,
+				testAttributes, goalAttribute);
 
-    /*
-     * Prints an item's guessed goal attribute value.
-     */
-    static private void printGuess(Item item, DecisionTree tree) {
-	AttributeSet itemAttributes = tree.getAttributeSet();
-	SymbolicAttribute goalAttribute = tree.getGoalAttribute();
-	
-	KnownSymbolicValue guessedGoalAttributeValue = 
-	    tree.guessGoalAttribute(item);
+		return builder.build().decisionTree();
+	}
 
-	String s = "The value guessed by the tree is " + 
-	    tree.getGoalAttribute().valueToString(guessedGoalAttributeValue);
-	
-	System.out.println(s);
-    }
+	/*
+	 * Prints a dot file content depicting a tree.
+	 */
+	static private void printDot(DecisionTree tree) {
+		System.out.println((new DecisionTreeToDot(tree)).produce());
+	}
+
+	/*
+	 * Prints an item's guessed goal attribute value.
+	 */
+	static private void printGuess(Item item, DecisionTree tree) {
+		AttributeSet itemAttributes = tree.getAttributeSet();
+		SymbolicAttribute goalAttribute = tree.getGoalAttribute();
+
+		KnownSymbolicValue guessedGoalAttributeValue = tree
+				.guessGoalAttribute(item);
+
+		String s = "The value guessed by the tree is "
+				+ tree.getGoalAttribute().valueToString(
+						guessedGoalAttributeValue);
+
+		System.out.println(s);
+	}
 
 }
